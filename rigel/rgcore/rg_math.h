e81d1c22914d54e22aba880d1a549fd334e95715
AprilCat <604241857@qq.com>
2017-09-14 01:29:20 +0800

    摸摸鱼

--- rigel/rgcore/rg_math.h
+++ rigel/rgcore/rg_math.h
@@ -1,29 +1,35 @@
 #pragma once
-
+#if defined(min)
+#undef min
+#endif
+#if defined(max)
+#undef max
+#endif
+#include <math.h>
 namespace rg {
-	typedef float RgScalar;
+	typedef float RgFloat;
 	struct RgVec2{
-		RgScalar x, y;
-		RgVec2(RgScalar _x,RgScalar _y):x(_x),y(_y){}
+		RgFloat x, y;
+		RgVec2(RgFloat _x,RgFloat _y):x(_x),y(_y){}
 		RgVec2():x(0),y(0){}
 		RgVec2(const RgVec2& v) :x(v.x), y(v.y) {}
 
-		RgVec2& operator +=(RgScalar s) {
+		RgVec2& operator +=(RgFloat s) {
 			x += s;
 			y += s;
 			return *this;
 		}
-		RgVec2& operator -=(RgScalar s) {
+		RgVec2& operator -=(RgFloat s) {
 			x -= s;
 			y -= s;
 			return *this;
 		}
-		RgVec2& operator *=(RgScalar s) {
+		RgVec2& operator *=(RgFloat s) {
 			x *= s;
 			y *= s;
 			return *this;
 		}
-		RgVec2& operator /=(RgScalar s) {
+		RgVec2& operator /=(RgFloat s) {
 			if (s != 0) {
 				x /= s;
 				y /= s;
@@ -46,25 +52,84 @@ namespace rg {
 			y *= v.y;
 			return *this;
 		}
-		
+		//等等等等，一脸懵逼
+		RgVec2& operator /=(const RgVec2& v) {
+			x /= v.x;
+			y /= v.y;
+			return *this;
+		}
+		RgFloat dot(const RgVec2& v) {
+			return x*v.x + y*v.y;
+		}
+		RgFloat length(){
+			return sqrt(this->dot(*this));
+		}
 	};
-
+	inline const RgFloat abs(RgFloat s) {
+		return abs((float)s);
+	}
+	inline const RgVec2 operator+(const RgVec2& v1, const RgFloat& s) {
+		return{ v1.x + s,v1.y + s };
+	}
+	inline const RgVec2 operator-(const RgVec2& v1, const RgFloat& s) {
+		return{ v1.x - s,v1.y - s };
+	}
+	inline const RgVec2 operator*(const RgVec2& v1, const RgFloat& s) {
+		return{ v1.x * s,v1.y * s };
+	}
+	inline const RgVec2 operator/(const RgVec2& v1, const RgFloat& s) {
+		if (s != 0) {
+			return{ v1.x / s,v1.y / s };
+		}
+		else{
+			return v1;
+		}
+	}
+	inline const RgVec2 operator+(const RgVec2& v1, const RgVec2& v2) {
+		return{ v1.x + v2.x,v1.y + v2.y };
+	}
+	inline const RgVec2 operator-(const RgVec2& v1, const RgVec2& v2) {
+		return{ v1.x - v2.x,v1.y - v2.y };
+	}
+	inline const RgVec2 operator*(const RgVec2& v1, const RgVec2& v2) {
+		return{ v1.x * v2.x,v1.y * v2.y };
+	}
+	//等等
+	inline const RgVec2 operator/(const RgVec2& v1, const RgVec2& v2) {
+		return{ v1.x / v2.x,v1.y / v2.y };
+	}
+	inline const RgFloat dot(const RgVec2& v1, const RgVec2& v2) {
+		return v1.x*v2.x + v1.y*v2.y;
+	}
+	inline const RgVec2 inverse(RgVec2 const& v){
+		return { -v.x,-v.y };
+	}
+	inline const RgFloat length(RgVec2 const& v) {
+		return sqrt(dot(v, v));
+	}
+	inline const RgVec2 min(RgVec2 const& v1, RgVec2 const& v2) {
+		return length(v1) < length(v2) ? v1 : v2;
+	}
+	inline const RgVec2 max(RgVec2 const& v1, RgVec2 const& v2) {
+		return length(v1) > length(v2) ? v1 : v2;
+	}
+	inline const RgVec2 abs(RgVec2 const& v){
+		return { abs(v.x),abs(v.y) };
+	}
 	struct RgVec3 {
-		RgScalar x = 0;
-		RgScalar y = 0;
-		RgScalar z = 0;
+		RgFloat x = 0;
+		RgFloat y = 0;
+		RgFloat z = 0;
 		RgVec3() :x(0), y(0), z(0) {}
-		RgVec3(RgScalar _x, RgScalar _y, RgScalar _z) :x(_x), y(_y), z(_z){}
+		RgVec3(RgFloat _x, RgFloat _y, RgFloat _z) :x(_x), y(_y), z(_z){}
 		inline RgVec3(const RgVec2& v) : x(v.x), y(v.y), z(0){}
 	};
 
 	struct RgVec4 {
-		RgScalar x, y, z, w;
+		RgFloat x, y, z, w;
 	};
 
-	inline const RgVec2 operator+(const RgVec2& v1, const RgVec2& v2) {
-		return{ v1.x + v2.x,v1.y + v2.y };
-	}
+
 
 	inline const RgVec3 operator+(const RgVec3& v1, const RgVec3& v2) {
 		return { v1.x + v2.x,v1.y + v2.y,v1.z + v2.z };
@@ -74,22 +139,18 @@ namespace rg {
 		return{ v1.x + v2.x,v1.y + v2.y,v1.z + v2.z,v1.z+v2.z};
 	}
 
-	inline const RgVec2 operator-(const RgVec2& v1, const RgVec2& v2) {
-		return{ v1.x - v2.x,v1.y - v2.y };
-	}
+	
 	inline const RgVec3 operator-(const RgVec3& v1, const RgVec3& v2) {
 		return{ v1.x - v2.x,v1.y - v2.y,v1.z - v2.z };
 	}
 	inline const RgVec4 operator-(const RgVec4& v1, const RgVec4& v2) {
 		return{ v1.x - v2.x,v1.y - v2.y,v1.z - v2.z,v1.z - v2.z };
 	}
-	inline const RgScalar operator*(const RgVec2& v1, const RgVec2& v2) {
-		return v1.x*v2.x + v1.y*v2.y;
-	}
-	inline const RgScalar operator*(const RgVec3& v1, const RgVec3& v2) {
+	
+	inline const RgFloat operator*(const RgVec3& v1, const RgVec3& v2) {
 		return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
 	}
-	inline const RgScalar operator*(const RgVec4& v1, const RgVec4& v2) {
+	inline const RgFloat operator*(const RgVec4& v1, const RgVec4& v2) {
 		return v1.w*v2.w + v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
 	}
 
