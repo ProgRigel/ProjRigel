f2680232e3c8e17669159bfed5c0926ca3e54817
yemi <zcyemi@gmail.com>
2017-10-14 20:58:38 +0800

    add rgtest

--- Rigel/rgtest/source/rgtest.cpp
+++ Rigel/rgtest/source/rgtest.cpp
@@ -3,7 +3,32 @@
 
 using namespace rg;
 
+static int count = 0;
+
+void test(){
+	if (count > 100) {
+
+		RgWindowManager::ReleaseWindow();
+	}
+
+	count++;
+}
+
 int main() {
+
+	RgWindow * win = nullptr;
+
+	RgWindowSettings set;
+	set.windowTitle = L"testWindow";
+
+	RgWindowManager::CreateWindowWithSettings(&win, &set);
+	win->Show();
+	//win->EventOnFrame.connect<&test>();
+	RgWindowManager::EnterRunLoop(10);
+
+
+
+	getchar();
 
 	return 0;
 }
